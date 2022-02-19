d91a88970b7808d5b647b50e6fa4c60cf4aae39e
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-02-19 09:17:04 +0600

    fwirst commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('border-id').addEventListener('click',function(){
+
+   const container=document.getElementById('friend-container');
+   container.style.border="10px solid green";
+
+})
