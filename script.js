5fc8428defbb39df894e7eb45db075aebd4768cf
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-02-19 09:30:03 +0600

    firfffst commit

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ document.getElementById('border-id').addEventListener('click',function(){
    container.style.border="10px solid green";
 
 })
+
+
+function addBackground(){
+const friends=document.getElementsByClassName('friends');
+
+for ( const friend of friends){
+    friend.style.backgroundColor='red';
+}
+
+}
